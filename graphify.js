514ce32fadae19c14c3181a6eaeb81e8040140cf
ArcadeFortune <alessionights@gmail.com>
2023-10-02 14:10:17 +0200

    dev: adding additive stats

--- graphify.js
+++ graphify.js
@@ -1,18 +1,6 @@
 // ChatGPT
-const graphify = (XML) => {
-  let data = [];
-  const observations = XML.querySelectorAll("Volume");
-  const startDateTime = XML.querySelector("StartDateTime").textContent;
-
-  observations.forEach((observation, index) => {
-    const sequence = observation.parentElement.querySelector("Sequence").textContent;
-    const volume = observation.textContent;
-    const dateTime = new Date(startDateTime);
-    dateTime.setMinutes(dateTime.getMinutes() + (parseInt(sequence) - 1) * 15);
-
-    const formattedDateTime = dateTime.toISOString().replace("Z", "");
-    data.push({ timestamp: formattedDateTime, value: volume });
-  });
+const graphify = (data, graphID) => {
+  
   // X- und Y-Achsendaten extrahieren
   const xData = data.map((entry) => new Date(entry.timestamp));
   const yData = data.map((entry) => entry.value);
@@ -40,5 +28,5 @@ const graphify = (XML) => {
   const plotData = [trace];
 
   // Plotly-Graph erstellen
-  Plotly.newPlot("graph", plotData, layout);
+  Plotly.newPlot("graph" + graphID, plotData, layout);
 };
