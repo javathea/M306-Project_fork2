6ec7bb6e22e8e83e2c9cb48fa018b935e2b5ba9a
anticN <anticn@bzz.ch>
2023-10-02 19:06:01 +0200

    fix: frontend with 1 .xml works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let XML;
 
 const readXML = () => {
-  fetch("sample.xml")
+  fetch("../sample.xml")
     .then((res) => {
       return res.text();
     })
@@ -14,3 +14,31 @@ const readXML = () => {
       graphify(additiveData, "additiveSdat");
     });
 };
+
+/*
+fs = require("fs");
+let XML;
+
+const readXML = () => {
+  fs.readdir("./SDAT-Files", (err, files) => {
+  
+    if(err) throw err;
+
+    files.forEach(file => {
+      fetch(file)
+      .then((res) => {
+      return res.text();
+    })
+    .then((String) => {
+      XML = new DOMParser().parseFromString(String, "text/xml");
+      convertToCSV(XML);
+      const additiveData = convertToAdditiveData(XML);
+      const data = convertToData(XML);
+      graphify(data, "sdat");
+      graphify(additiveData, "additiveSdat");
+    })
+    })
+  }
+)};
+
+*/
