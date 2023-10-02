6ccbe31261598f038b6a37704cdea32142b67688
ArcadeFortune <alessionights@gmail.com>
2023-10-02 13:06:25 +0200

    dev: can convert to csv for the chart

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+
 var XML;
 
 const readXML = () => {
-  fetch("sample.xml").then((res) => { // fetche
+  fetch("sample.xml").then((res) => {
       return res.text();
     })
     .then((String) => {
-      XML = new DOMParser().parseFromString(String, "text/xml"); // parse
+      XML = new DOMParser().parseFromString(String, "text/xml");
       console.log(XML);
+
+      const observations = XML.querySelectorAll("Volume");
+      const startDateTime = XML.querySelector("StartDateTime").textContent;
+      convertToCSV(observations, startDateTime);
     });
 };
