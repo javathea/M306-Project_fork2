4e076c5ee8b860fbfdd7d76aab6fd78c5e81fbb5
ArcadeFortune <alessionights@gmail.com>
2023-10-02 13:52:09 +0200

    feat: added plotable graph

--- convertToCSV.js
+++ convertToCSV.js
@@ -1,16 +1,18 @@
 // ChatGPT
-const convertToCSV = (observations, startTime) => {
-  
+const convertToCSV = (XML) => {
+  const observations = XML.querySelectorAll("Volume");
+  const startDateTime = XML.querySelector("StartDateTime").textContent;
+
   // CSV-Header erstellen
   let csvData = "DateTime,Volume\n";
 
   observations.forEach((observation, index) => {
     const sequence = observation.parentElement.querySelector("Sequence").textContent;
     const volume = observation.textContent;
-    const dateTime = new Date(startTime);
+    const dateTime = new Date(startDateTime);
     dateTime.setMinutes(dateTime.getMinutes() + (parseInt(sequence) - 1) * 15);
 
-    const formattedDateTime = dateTime.toISOString().replace("T", "").replace("Z", "");
+    const formattedDateTime = dateTime.toISOString().replace("Z", "");
     csvData += `${formattedDateTime},${volume}\n`;
 
     console.log(csvData);
---
+++ graphify.js
@@ -0,0 +1,44 @@
+// ChatGPT
+const graphify = (XML) => {
+  let data = [];
+  const observations = XML.querySelectorAll("Volume");
+  const startDateTime = XML.querySelector("StartDateTime").textContent;
+
+  observations.forEach((observation, index) => {
+    const sequence = observation.parentElement.querySelector("Sequence").textContent;
+    const volume = observation.textContent;
+    const dateTime = new Date(startDateTime);
+    dateTime.setMinutes(dateTime.getMinutes() + (parseInt(sequence) - 1) * 15);
+
+    const formattedDateTime = dateTime.toISOString().replace("Z", "");
+    data.push({ timestamp: formattedDateTime, value: volume });
+  });
+  // X- und Y-Achsendaten extrahieren
+  const xData = data.map((entry) => new Date(entry.timestamp));
+  const yData = data.map((entry) => entry.value);
+
+  // Layout-Einstellungen für den Graphen
+  const layout = {
+    xaxis: {
+      type: "date", // X-Achse als Zeitachse
+      title: "Zeit",
+      rangeslider: {}, // Range-Slider unter dem Graphen
+    },
+    yaxis: {
+      title: "Volumes",
+    },
+  };
+
+  // Trace für die Daten erstellen
+  const trace = {
+    x: xData,
+    y: yData,
+    type: "scatter", // Linien-Diagramm
+  };
+
+  // Daten-Array für Plotly erstellen
+  const plotData = [trace];
+
+  // Plotly-Graph erstellen
+  Plotly.newPlot("graph", plotData, layout);
+};
