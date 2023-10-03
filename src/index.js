4a6edb27b5a6beebf3b22d15684072039fa82e26
anticN <anticn@bzz.ch>
2023-10-03 08:54:21 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 let XML;
 
-const readXML = () => {
-  fetch("sample.xml")
-    .then((res) => {
-      return res.text();
-    })
-    .then((String) => {
-      XML = new DOMParser().parseFromString(String, "text/xml");
+//const readXML = async () => {
+//  await fetch("http://localhost:3000/sdat", {method: "GET"})
+    /*.then((res) => {
+      res.text();
+    })*/
+//    .then((res) => {
+//      console.log(res);
+      /*XML = new DOMParser().parseFromString(String, "text/xml");
       convertToCSV(XML);
       const additiveData = convertToAdditiveData(XML);
-      const data = convertToData(XML);
-      graphify(data, "sdat");
-      graphify(additiveData, "additiveSdat");
+      const data = convertToData(XML);*/
+      //graphify(res, "sdat");
+      //graphify(additiveData, "additiveSdat");
+//    })
+/*    .catch((err) => {
+      console.log(err);
     });
 };
+*/
+
+function readXML(){
+  try {
+    const response = fetch('http://127.0.0.1:5000/sdat', {method: "GET"});  // Fetching data from the /sdat route
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = response.json();  // Parsing the response as JSON
+    console.log('Received data:', data);  // Do something with the received data
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
