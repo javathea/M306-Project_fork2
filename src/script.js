f4db1003644aa0c8309f9461bbecd29415689252
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-10-05 16:06:25 +0200

    feat: Push für ava

--- src/script.js
+++ src/script.js
@@ -3,18 +3,23 @@ function handleFileUpload(event) {
     const formData = new FormData();
 
     for (const file of files) {
-        // Check if the file extension is .sdat or .esl
         const allowedExtensions = ['sdat', 'esl', 'xml'];
         const fileExtension = file.name.split('.').pop().toLowerCase();
 
         if (allowedExtensions.includes(fileExtension)) {
-            formData.append('files', file);
+            if (/^\d/.test(file.name)) {
+                // Files starting with a number go to "uploads" directory
+                formData.append('files', file, `uploads/${file.name}`);
+            } else {
+                // Other files go to "SDAT-Files" directory
+                formData.append('files', file, `SDAT-Files/${file.name}`);
+            }
         } else {
-            console.warn(`File ${file.name} is not a valid XML file (sdat or esl). Skipping...`);
-            alert(`File ${file.name} is not a valid XML file (sdat or esl). It will be skipped.`);
+            console.warn(`File ${file.name} is not a valid XML file (sdat, esl, or xml). Skipping...`);
+            alert(`File ${file.name} is not a valid XML file (sdat, esl, or xml). It will be skipped.`);
         }
     }
-  
+
     if (formData.getAll('files').length > 0) {
         fetch('http://localhost:3001/upload', {
             method: 'POST',
@@ -30,6 +35,19 @@ function handleFileUpload(event) {
         alert('No valid XML files selected for upload.');
     }
 }
+
+document.getElementById('delete-files').addEventListener('click', () => {
+    fetch('http://localhost:3001/deleteFiles', {
+      method: 'POST',
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Server response:', data);
+    })
+    .catch(error => console.error('Error deleting files:', error));
+  });
+
+
 
 const fileInput = document.getElementById('file-input');
 fileInput.addEventListener('change', handleFileUpload);
